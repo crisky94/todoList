948415a4376d0e47811632f888fe684402b94b9b
Cris <101344948+crisky94@users.noreply.github.com>
2024-10-08 19:48:01 +0200

    togglecomplete

--- src/components/Tarea.tsx
+++ src/components/Tarea.tsx
@@ -1,16 +1,21 @@
 type Tarea = {
   tarea: string
+  completada: boolean;
   borrarTarea: () => void
+  toggleCompletada: () => void; 
 }
 
-export const Tarea = ({tarea, borrarTarea}: Tarea ) => {
+export const Tarea = ({tarea, borrarTarea, completada, toggleCompletada}: Tarea ) => {
 
   return (
     <div className="task">
       <label className="custom-checkbox">
-      <input type="checkbox"/>
+      <input type="checkbox"
+          checked={completada} 
+          onChange={toggleCompletada} 
+        />
       <span className="checkmark"></span>
-      <span className="task-text">{tarea}</span>
+      <span className="{`task-text ${completada ? 'completed' : ''}`}">{tarea}</span>
     </label>
       <button className="delete-btn" onClick={borrarTarea}>Borrar</button>
     </div>
